6043726e22e4d9e1839b0ee008ae908e4ec97f27
dilipvaleti <57723484+dilipvaleti@users.noreply.github.com>
2021-12-15 21:21:18 +0530

    Adding code to prepare Titanic survival api

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# titanic_api
-Creating this repository to create Titanic survival API
+# Deploy ML model with Flask to Heroku
+
+Click the button below to quickly clone and deploy into your own Heroku acount.
+If you don't have one it'll prompt you to setup a free one.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+Once deployed to your Heroku instance run the following:
+
+```bash
+curl -s -XPOST 'https://<name-of-your-heroku-app>.herokuapp.com/predict/982' -H 'accept-content: application/json'
+```
+
+Alternatively a simple python script:
+
+```python
+import requests
+import json
+url = 'https://<name-of-your-heroku-app>.herokuapp.com/predict/982'
+response = requests.post(url)
+print(response.json())
+```
---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from flask import Flask, jsonify, request
+import pickle
+
+# load model
+model = pickle.load(open('model.pkl','rb'))
+
+# app
+app = Flask(__name__)
+
+@app.route('/predict/<int:pass_id>', methods=['POST'])
+def predict(pass_id: int):
+    # get data
+    data = pd.read_csv('test.csv')
+    features = ['Pclass', 'Age', 'SibSp', 'Fare', 'Parch']
+    pass_info = data[data['PassengerId'] == pass_id][features]
+    if len(pass_info) == 0:
+        return jsonify(Error="Given passenger ID doesn't exist so please enter in between 892 and 1309")
+    pass_status = model.predict(pass_info)
+    status = {1: 'Survived', 0: 'Not Survived'}
+    return jsonify(status=status[pass_status[0]])
+
+
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
+
